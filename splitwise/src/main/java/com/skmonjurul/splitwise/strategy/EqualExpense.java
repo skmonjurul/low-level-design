4973c3177c99dfb46cfec66fba53e91a21db117d
skmonjurul <monjurulhaque2011@gmail.com>
2023-02-17 16:41:27 +0530

    added concrete class for Expense

---
+++ splitwise/src/main/java/com/skmonjurul/splitwise/strategy/EqualExpense.java
@@ -0,0 +1,8 @@
+package com.skmonjurul.splitwise.strategy;
+
+public class EqualExpense extends Expense{
+
+    public EqualExpense(SplitBehaviour splitBehaviour) {
+        this.splitBehaviour = splitBehaviour;
+    }
+}
---
+++ splitwise/src/main/java/com/skmonjurul/splitwise/strategy/UnEqualExpense.java
@@ -0,0 +1,8 @@
+package com.skmonjurul.splitwise.strategy;
+
+public class UnEqualExpense extends Expense{
+
+    public UnEqualExpense(SplitBehaviour splitBehaviour) {
+        this.splitBehaviour = splitBehaviour;
+    }
+}
